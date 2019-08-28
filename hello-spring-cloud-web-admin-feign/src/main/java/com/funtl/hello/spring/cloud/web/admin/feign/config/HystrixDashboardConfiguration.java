04a793f4884fbc1d2015d199f5d0320ab6932f0e
chenming <543428992@qq.com>
2019-08-28 14:00:37 +0800

    first commit

---
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/config/HystrixDashboardConfiguration.java
@@ -0,0 +1,23 @@
+package com.funtl.hello.spring.cloud.web.admin.feign.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixDashboardConfiguration {
+
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet=new HystrixMetricsStreamServlet();
+        ServletRegistrationBean servletRegistrationBean= new ServletRegistrationBean(streamServlet);
+        servletRegistrationBean.setLoadOnStartup(1);
+        servletRegistrationBean.addUrlMappings("/hystrix.stream");
+        servletRegistrationBean.setName("HystrixMetricsStreamServlet");
+        return servletRegistrationBean;
+    }
+
+
+}
